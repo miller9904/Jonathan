8312ffc9f9a226d14fab8abb4be3505b6e184b89
M Miller <miller9904@gmail.com>
2014-09-23 17:50:47 -0400

    Initial client client files commit

---
+++ js/phrases.js
@@ -0,0 +1,40 @@
+// Jonathan's premade responses
+var phrases = {
+	WHO_ARE_YOU: [
+		"I'm Jonathan, your personal assistant.",
+		"I'm your personal assistant.",
+		"I'm Jonathan.",
+		"My name is Jonathan.",
+		"Jonathan."
+	],
+	
+	UNDERSTAND_ERROR: [
+		"I'm sorry, I can't understand what you said.",
+		"What?",
+		"What was that you said?",
+		"I can't understand.  Sorry.",
+		"Could you rephrase that?"
+	],
+	
+	//Easter Egg Responses
+	
+	WHERE_DO_YOU_LIVE: [
+		"I'll give you a hint: I'm surrounded by silicon.",
+		"Up in the cloud."
+	],
+	
+	WHO_MADE_YOU: [
+		"Two nerds.  They're still teaching me stuff.",
+		"Geeks.  Can't you tell?"
+	],
+	
+	BEST_FOOD: [
+		"Well, I can't say that I know.",
+		"Hard to say.  What's yours?"
+	],
+	
+	WORST_FOOD: [
+		"I hate tag soup.",
+		"My least favorite food is tag soup, but if you like it, that's fine."
+	]
+}
